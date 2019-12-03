2e94f2f992c7be47accc2cda6d17d3b5078fe32c
Albert Pizarro <piklerogames@gmail.com>
2019-12-03 20:02:00 +0100

    Player, Enemies

---
+++ Actividades/src/G01_Pizarro_Mateu_AA2/Clyde.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+#include "Enemies.h"
+
+class Clyde : Enemies
+{
+	//public Move(Direction, Map);
+};
---
+++ Actividades/src/G01_Pizarro_Mateu_AA2/Inky.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+#include "Enemies.h"
+
+class Inky : Enemies
+{
+	//void Move(Direction, Map);
+};
